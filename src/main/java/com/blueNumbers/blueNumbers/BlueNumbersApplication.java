69caa5f4bbffa73f77947276f86d6d40fe330b66
Norbert2711 <norbertu2711@gmail.com>
2021-02-09 20:24:33 +0100

    start

---
+++ src/main/java/com/blueNumbers/blueNumbers/BlueNumbersApplication.java
@@ -0,0 +1,13 @@
+package com.blueNumbers.blueNumbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueNumbersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueNumbersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blueNumbers/blueNumbers/BlueNumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueNumbers.blueNumbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueNumbersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
